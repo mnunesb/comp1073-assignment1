23635f7675fbf0e3e108033d13b5b39a0661ad2a
Marcello Nunes Bernardes <mnunesbernardes@gmail.com>
2022-02-17 20:01:21 -0500

    Initial files push

---
+++ js/random_sentence.js.js
@@ -0,0 +1,47 @@
+/* Global variables
+-------------------------------------------------- */
+
+// Buttons
+
+
+// Paragraphs for output
+
+
+// Word lists
+
+
+// Variables to store selected word/phrase
+
+
+/* Functions
+-------------------------------------------------- */
+
+// Set the noun
+
+
+// Set the verb
+
+
+// Set the adjective
+
+
+// Set the second noun
+
+
+// Set the setting for the story
+
+
+// Output the completed sentence to the display paragraph
+
+
+// Output the completed random sentence to the display paragraph
+
+
+// Random number function for 'Surprise Me' button
+
+
+
+/* Event handlers
+-------------------------------------------------- */
+
+// Buttons to pick words/phrases
