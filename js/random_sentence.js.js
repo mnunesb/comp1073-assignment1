1d6d93b2abfdc0df67409b4130fd4b105745c76d
Marcello Nunes Bernardes <mnunesbernardes@gmail.com>
2022-02-18 21:32:53 -0500

    Added comments to the code

--- js/random_sentence.js.js
+++ js/random_sentence.js.js
@@ -1,25 +1,29 @@
 /* Global variables
 -------------------------------------------------- */
 
-// Buttons
-var button1 = document.getElementById('noun1');
-var button2 = document.getElementById('verb');
-var button3 = document.getElementById('adjective');
-var button4 = document.getElementById('noun2');
-var button5 = document.getElementById('setting');
-var btn1Indx = 0;
-var btn2Indx = 0;
-var btn3Indx = 0;
-var btn4Indx = 0;
-var btn5Indx = 0;
+// Word Buttons
+let button1 = document.getElementById('noun1');
+let button2 = document.getElementById('verb');
+let button3 = document.getElementById('adjective');
+let button4 = document.getElementById('noun2');
+let button5 = document.getElementById('setting');
+
+//Word buttons indexes
+let btn1Indx = 0;
+let btn2Indx = 0;
+let btn3Indx = 0;
+let btn4Indx = 0;
+let btn5Indx = 0;
 
 
 // Paragraphs for output
-var noun1P = document.getElementById('selectedNoun1');
-var verbP = document.getElementById('selectedVerb');
-var adjectiveP = document.getElementById('selectedAdjective');
-var noun2P = document.getElementById('selectedNoun2');
-var settingP = document.getElementById('selectedSetting');
+let noun1P = document.getElementById('selectedNoun1');
+let verbP = document.getElementById('selectedVerb');
+let adjectiveP = document.getElementById('selectedAdjective');
+let noun2P = document.getElementById('selectedNoun2');
+let settingP = document.getElementById('selectedSetting');
+
+//Page buttons functions and listeners
 let manualPhrase = document.getElementById('viewSentence');
 let display = document.getElementById('yourPhrase');
 let randomPhrase = document.getElementById('randomSentence');
@@ -31,13 +35,13 @@ let verb = ["sat on", "ate", "danced with", "saw", "doesn't like","kissed"];
 let adjective = ["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"];
 let noun2 =["goat", "monkey", "fish", "cow", "frog", "bug", "worm"];
 let setting = ["on the moon", "on the chair", "in my spaghetti", "in my soup", "on the grass", "in my shoes"];
-// Variables to store selected word/phrase
 
 
 /* Functions
 -------------------------------------------------- */
+//selectWord takes the number of times the user has clicked on one of the words buttons and cycles through the elements of that array and displays the selection to the user
 function selectWord (id) {
-    var buttonId = id;
+    let buttonId = id;
     switch (buttonId) {
         case 'noun1':
             if(btn1Indx > noun1.length-1){
@@ -79,17 +83,20 @@ function selectWord (id) {
     }
 }
 
+//createStory displays all the elements selected by the user as a continuous phrase
 function createStory() {
     yourPhrase =[noun1[btn1Indx-1],verb[btn2Indx-1], adjective[btn3Indx-1], noun2[btn4Indx-1], setting[btn5Indx-1]];
     display.textContent = yourPhrase.join(' ');
 }
 
+//randomStory generates a random phrase by randomly selecting a word from each word arrray
 function randomStory() {
 
     yourPhrase =[noun1[Math.floor(Math.random() * noun1.length)],verb[Math.floor(Math.random() * verb.length)], adjective[Math.floor(Math.random() * adjective.length)], noun2[Math.floor(Math.random() * noun2.length)], setting[Math.floor(Math.random() * setting.length)]];
     display.textContent = yourPhrase.join(' ');
 }
 
+//resetPhrase resets all arrays so the user can start over
 function resetPhrase (){
     noun1P.textContent ='';
     verbP.textContent ='';
